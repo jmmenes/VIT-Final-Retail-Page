cffd2fa8ee14c45c066ad15b0322cd8a4631313b
John Meneses <jmmenes90@gmail.com>
2021-08-12 19:28:06 -0400

    Need to figure out cart UI & cart functionality increment/decerement price. Include add to cart functionality.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Smooth Scroll To Reviews
+// Smooth Scroll Button To Reviews
 
 let btn = document.querySelector('.btn');
 let element = document.querySelector('.scroll');
@@ -7,4 +7,26 @@ btn.addEventListener('click', function () {
   element.scrollIntoView({ behavior: 'smooth' });
 });
 
-// Cart Modal
+///////////////////////////////////////////////////////////
+
+// Display/Close cart modal, click function
+
+const modalBtn = document.querySelector('.modal-btn');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+
+modalBtn.addEventListener('click', function () {
+  modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener('click', function () {
+  modalBg.classList.remove('bg-active');
+});
+
+///////////////////////////////////////////////////////////
+
+// Add item to cart, click function
+
+// remove item from cart, click function
+
+// increment, decrement product price
